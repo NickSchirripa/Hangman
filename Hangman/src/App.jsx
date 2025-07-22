7ede15802bb6540c01d6e4fb6ebc9cbc051fa35f
Nick Schirripa <108542730+NickSchirripa@users.noreply.github.com>
2025-07-22 17:05:29 -0400

    722

--- Hangman/src/App.jsx
+++ Hangman/src/App.jsx
@@ -1,18 +1,52 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Lives from "./Lives.jsx";
-import { lifeData } from './lifeData.js';
+import { lifeData } from "./lifeData.js";
+import { nanoid } from "nanoid";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
+  const [currentWord, setCurrentWord] = useState("hangman");
+
+  //state to hold array when keyboard is clicked
+  const [letterClicked, setLetterClicked] = useState([]);
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+   function addGuessedLetter(letter) {
+        setLetterClicked(prevLetters => prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter])
+    }
+
+  //render Live component, and pass in props
   const lifeElement = lifeData.map((life, index) => (
-    <Lives 
+    <Lives
       key={index}
       name={life.name}
       backgroundColor={life.backgroundColor}
       color={life.color}
     />
   ));
+
+  //render our word element
+  const currentWordElement = currentWord
+    .split("")
+    .map((word) => <span key={nanoid()}>{word.toUpperCase()}</span>);
+
+  //map over our alphabet variable, and turn them into buttons for the keyboard
+  const keyboardElement = alphabet.split("").map((letters) =>{
+    /////////////////NEXT STEP/////////////////////
+    //Check to see if the letter was guessed  and check to see if it is in the current 
+    //word. Then change its class so the background changes colors
+    return(
+    <button
+      onClick={()=> addGuessedLetter(letters)}
+      key={nanoid()}
+    >
+      {letters}
+    </button>
+  )});
+
+  console.log(letterClicked);
 
   return (
     <>
@@ -21,9 +55,10 @@ export default function App() {
           <h1>HangMan</h1>
           <p>Guess the word within 8 attempts!</p>
         </header>
-        <div className="chipsCntr">
-          {lifeElement}
-        </div>
+        <section className="chipsCntr">{lifeElement}</section>
+        <section className="currentWord">{currentWordElement}</section>
+        <section className="keyboard">{keyboardElement}</section>
+        <button className="new-game">New Game</button>
       </main>
     </>
   );
